77a53fcba16aae53a932d7ef89aa650982be7393
Francisco Rafart <francisco.rafart@gmail.com>
2018-05-10 21:02:47 -0400

    Remove comments

--- euler31.js
+++ euler31.js
@@ -1,9 +1,4 @@
-//solve using Ramda
-const R = require('ramda')
-
 let values = [1,2,5,10,20,50,100,200]
-
-//200 coins is the max
 
 //for each number loop through the thing starting at one each time,
 let giveOneComb = (ind, values) => {
